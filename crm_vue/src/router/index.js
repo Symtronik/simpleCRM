9955d04ea11d8671f9df9145b0c535160bed28d6
staz <m.kobialka@healthpharm.pl>
2023-09-23 17:18:47 +0200

    Created a page for log in

--- crm_vue/src/router/index.js
+++ crm_vue/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 import SignUpView from '../views/SignUpView.vue'
+import LogInView from '../views/LogInView.vue'
 
 const routes = [
   {
@@ -11,8 +12,13 @@ const routes = [
   },
   {
     path: '/sign-up',
-    name: 'SignUp',
+    name: 'SignUpView',
     component: SignUpView
+  },
+  {
+    path: '/log-in',
+    name: 'LogInView',
+    component: LogInView
   },
   {
     path: '/about',
