8fe2f96bdb2bf5e57d5260b3dea0bd6bd941414f
staz <m.kobialka@healthpharm.pl>
2023-09-23 16:47:41 +0200

    Add layout navbar

--- crm_vue/src/App.vue
+++ crm_vue/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Navbar/>
   <router-view/>
 </template>
+
+<script>
+    import Navbar from '@/components/layout/Navbar'
+
+    export default{
+      name: 'App',
+      components:{
+        Navbar
+      }
+    }
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma';
---
+++ crm_vue/src/components/layout/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar is-dark">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item">
+                <strong>CRM</strong>
+            </router-link>
+        </div>
+
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <route-link to="/sign-up" class="button is-success"><strong>Sign up</strong></route-link>
+                        <route-link to="/sign-up" class="button is-success"><strong>Log in</strong></route-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+
+</script>
+
